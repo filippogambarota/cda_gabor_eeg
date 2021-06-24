853e529abdc8f511613eee955e052db7844ed9ca
Filippo Gambarota <filippo.gambarota@gmail.com>
2021-06-24 11:02:36 +0200

    adding cue and target trigger from the R script

--- make_cond/make_cond.R
+++ make_cond/make_cond.R
@@ -5,13 +5,13 @@ library(tidyverse)
 # Generate Conditions
 
 cue <- c("left", "right")
-orientations <- c(1, 2, 3, 4, 5, 6, 7)
+target_orientation <- c(1, 2, 3, 4, 5, 6, 7)
 change <- c("yes", "no")
 ntrials <- 1
 
 valid_trials <- expand_grid(
     cue,
-    orientations,
+    target_orientation,
     trial_type = "valid",
     change,
     trial = 1:ntrials
@@ -19,7 +19,7 @@ valid_trials <- expand_grid(
 
 catch_trials <- expand_grid(
     cue,
-    orientations = 0,
+    target_orientation = 0,
     trial_type = "catch",
     change = 0,
     trial = 1:10
@@ -27,9 +27,20 @@ catch_trials <- expand_grid(
 
 exp_trials <- rbind(valid_trials, catch_trials)
 exp_trials$trial <- 1:nrow(exp_trials)
+exp_trials <- select(exp_trials, -trial)
+
+# Test for triggers
 
 exp_trials %>% 
-    select(-trial) %>% 
+
+    # TRIGGERS MUTATE
+
+    mutate(cue_trigger = ifelse(cue == "left", 50, 59),
+           target_trigger = ifelse(trial_type == "valid",
+                                   100 + target_orientation,
+                                   199)) -> exp_trials
+
+exp_trials %>% 
     write.table(., file = "exp_cond.txt",
                 sep = ",", # separator
                 eol = ",", # for having all in a single line sep
